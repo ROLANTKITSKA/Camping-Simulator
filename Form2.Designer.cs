133ccc15007bd0f725e73af14b05e50dd53d3c09
ROLANTKITSKA <115018154+ROLANTKITSKA@users.noreply.github.com>
2025-01-26 19:41:13 +0200

    Create Form2.Designer.cs

---
+++ Form2.Designer.cs
@@ -0,0 +1,282 @@
+namespace Ergasia_Allilepidrasi
+{
+    partial class Form2
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            panel1 = new Panel();
+            button5 = new Button();
+            label7 = new Label();
+            button4 = new Button();
+            button3 = new Button();
+            button2 = new Button();
+            button1 = new Button();
+            comboBox4 = new ComboBox();
+            comboBox3 = new ComboBox();
+            comboBox2 = new ComboBox();
+            comboBox1 = new ComboBox();
+            label6 = new Label();
+            label5 = new Label();
+            label4 = new Label();
+            label3 = new Label();
+            label2 = new Label();
+            panel2 = new Panel();
+            label1 = new Label();
+            panel1.SuspendLayout();
+            panel2.SuspendLayout();
+            SuspendLayout();
+            // 
+            // panel1
+            // 
+            panel1.BackColor = SystemColors.ActiveCaption;
+            panel1.Controls.Add(button5);
+            panel1.Controls.Add(label7);
+            panel1.Controls.Add(button4);
+            panel1.Controls.Add(button3);
+            panel1.Controls.Add(button2);
+            panel1.Controls.Add(button1);
+            panel1.Controls.Add(comboBox4);
+            panel1.Controls.Add(comboBox3);
+            panel1.Controls.Add(comboBox2);
+            panel1.Controls.Add(comboBox1);
+            panel1.Controls.Add(label6);
+            panel1.Controls.Add(label5);
+            panel1.Controls.Add(label4);
+            panel1.Controls.Add(label3);
+            panel1.Controls.Add(label2);
+            panel1.Controls.Add(panel2);
+            panel1.Location = new Point(1, 1);
+            panel1.Name = "panel1";
+            panel1.Size = new Size(926, 525);
+            panel1.TabIndex = 0;
+            // 
+            // button5
+            // 
+            button5.Location = new Point(625, 424);
+            button5.Name = "button5";
+            button5.Size = new Size(120, 57);
+            button5.TabIndex = 16;
+            button5.Text = "On-line Help";
+            button5.UseVisualStyleBackColor = true;
+            button5.Click += button5_Click;
+            // 
+            // label7
+            // 
+            label7.Location = new Point(459, 337);
+            label7.Name = "label7";
+            label7.Size = new Size(137, 39);
+            label7.TabIndex = 15;
+            label7.Text = "Λογαριασμός: ";
+            label7.TextAlign = ContentAlignment.MiddleCenter;
+            // 
+            // button4
+            // 
+            button4.Location = new Point(476, 424);
+            button4.Name = "button4";
+            button4.Size = new Size(120, 57);
+            button4.TabIndex = 14;
+            button4.Text = "Chat";
+            button4.UseVisualStyleBackColor = true;
+            button4.Click += button4_Click;
+            // 
+            // button3
+            // 
+            button3.Location = new Point(328, 424);
+            button3.Name = "button3";
+            button3.Size = new Size(120, 57);
+            button3.TabIndex = 13;
+            button3.Text = "Pay";
+            button3.UseVisualStyleBackColor = true;
+            button3.Click += button3_Click;
+            // 
+            // button2
+            // 
+            button2.Location = new Point(180, 424);
+            button2.Name = "button2";
+            button2.Size = new Size(120, 57);
+            button2.TabIndex = 12;
+            button2.Text = "Order Status";
+            button2.UseVisualStyleBackColor = true;
+            button2.Click += button2_Click;
+            // 
+            // button1
+            // 
+            button1.Location = new Point(31, 424);
+            button1.Name = "button1";
+            button1.Size = new Size(121, 57);
+            button1.TabIndex = 11;
+            button1.Text = "Place Order";
+            button1.UseVisualStyleBackColor = true;
+            button1.Click += button1_Click;
+            // 
+            // comboBox4
+            // 
+            comboBox4.FormattingEnabled = true;
+            comboBox4.Items.AddRange(new object[] { "Εσπρέσο", "Καπουτσίνο", "Λάττε", "Φρέντο", "Φραπέ" });
+            comboBox4.Location = new Point(229, 348);
+            comboBox4.Name = "comboBox4";
+            comboBox4.Size = new Size(151, 28);
+            comboBox4.TabIndex = 10;
+            comboBox4.SelectedIndexChanged += comboBox4_SelectedIndexChanged;
+            // 
+            // comboBox3
+            // 
+            comboBox3.FormattingEnabled = true;
+            comboBox3.Items.AddRange(new object[] { "Κόκα-Κόλα", "Φάντα", "Νερό", "Μπύρα", "Κρασί" });
+            comboBox3.Location = new Point(229, 311);
+            comboBox3.Name = "comboBox3";
+            comboBox3.Size = new Size(151, 28);
+            comboBox3.TabIndex = 9;
+            comboBox3.SelectedIndexChanged += comboBox3_SelectedIndexChanged;
+            // 
+            // comboBox2
+            // 
+            comboBox2.FormattingEnabled = true;
+            comboBox2.Location = new Point(229, 274);
+            comboBox2.Name = "comboBox2";
+            comboBox2.Size = new Size(151, 28);
+            comboBox2.TabIndex = 8;
+            comboBox2.SelectedIndexChanged += comboBox2_SelectedIndexChanged;
+            // 
+            // comboBox1
+            // 
+            comboBox1.FormattingEnabled = true;
+            comboBox1.Items.AddRange(new object[] { "Πρωινό", "Μεσημεριανό", "Βραδινό" });
+            comboBox1.Location = new Point(229, 235);
+            comboBox1.Name = "comboBox1";
+            comboBox1.Size = new Size(151, 28);
+            comboBox1.TabIndex = 7;
+            comboBox1.SelectedIndexChanged += comboBox1_SelectedIndexChanged;
+            // 
+            // label6
+            // 
+            label6.AutoSize = true;
+            label6.Location = new Point(31, 356);
+            label6.Name = "label6";
+            label6.Size = new Size(106, 20);
+            label6.TabIndex = 6;
+            label6.Text = "Επιλέξτε καφέ:";
+            label6.TextAlign = ContentAlignment.MiddleCenter;
+            // 
+            // label5
+            // 
+            label5.AutoSize = true;
+            label5.Location = new Point(31, 319);
+            label5.Name = "label5";
+            label5.Size = new Size(110, 20);
+            label5.TabIndex = 5;
+            label5.Text = "Επιλέξτε ποτό: ";
+            label5.TextAlign = ContentAlignment.MiddleCenter;
+            // 
+            // label4
+            // 
+            label4.AutoSize = true;
+            label4.Location = new Point(31, 282);
+            label4.Name = "label4";
+            label4.Size = new Size(117, 20);
+            label4.TabIndex = 4;
+            label4.Text = "Επιλέξτε γεύμα: ";
+            label4.TextAlign = ContentAlignment.MiddleCenter;
+            // 
+            // label3
+            // 
+            label3.AutoSize = true;
+            label3.Location = new Point(31, 243);
+            label3.Name = "label3";
+            label3.Size = new Size(180, 20);
+            label3.TabIndex = 3;
+            label3.Text = "Επιλέξτε είδος γεύματος: ";
+            label3.TextAlign = ContentAlignment.MiddleCenter;
+            // 
+            // label2
+            // 
+            label2.AutoSize = true;
+            label2.Font = new Font("Segoe UI", 12F, FontStyle.Regular, GraphicsUnit.Point, 161);
+            label2.Location = new Point(31, 169);
+            label2.Name = "label2";
+            label2.Size = new Size(428, 28);
+            label2.TabIndex = 2;
+            label2.Text = "Παρακαλώ τοποθετήστε την παραγγελία σας!";
+            label2.TextAlign = ContentAlignment.MiddleCenter;
+            // 
+            // panel2
+            // 
+            panel2.BackColor = SystemColors.ActiveCaptionText;
+            panel2.Controls.Add(label1);
+            panel2.Location = new Point(0, 0);
+            panel2.Name = "panel2";
+            panel2.Size = new Size(926, 145);
+            panel2.TabIndex = 1;
+            // 
+            // label1
+            // 
+            label1.Font = new Font("Segoe MDL2 Assets", 18F, FontStyle.Regular, GraphicsUnit.Point, 0);
+            label1.ForeColor = SystemColors.ButtonHighlight;
+            label1.Location = new Point(156, 19);
+            label1.Name = "label1";
+            label1.Size = new Size(622, 89);
+            label1.TabIndex = 0;
+            label1.Text = "Καλώς Ήρθατε στο Εστιατόριο της Κατασκήνωσης";
+            label1.TextAlign = ContentAlignment.MiddleCenter;
+            // 
+            // Form2
+            // 
+            AutoScaleDimensions = new SizeF(8F, 20F);
+            AutoScaleMode = AutoScaleMode.Font;
+            ClientSize = new Size(928, 522);
+            Controls.Add(panel1);
+            Name = "Form2";
+            Text = "Form2";
+            Load += Form2_Load;
+            panel1.ResumeLayout(false);
+            panel1.PerformLayout();
+            panel2.ResumeLayout(false);
+            ResumeLayout(false);
+        }
+
+        #endregion
+
+        private Panel panel1;
+        private Label label1;
+        private Panel panel2;
+        private Label label2;
+        private Label label6;
+        private Label label5;
+        private Label label4;
+        private Label label3;
+        private ComboBox comboBox4;
+        private ComboBox comboBox3;
+        private ComboBox comboBox2;
+        private ComboBox comboBox1;
+        private Button button1;
+        private Button button3;
+        private Button button2;
+        private Button button4;
+        private Label label7;
+        private Button button5;
+    }
+}
